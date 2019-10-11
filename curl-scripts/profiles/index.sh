7ea519e49fd955bf7ea4ed31244b250288dd1796
gbbenson68 <gbbenson68@gmail.com>
2019-10-11 10:22:41 -0400

    Change PROF_TOKEN to USER_TOKEN

--- curl-scripts/profiles/index.sh
+++ curl-scripts/profiles/index.sh
@@ -8,7 +8,7 @@ then
   echo '  NOTE: Although the BASE_URL is the only variable specified on the command line, the'
   echo '        following variables must also be set in the environment:'
   echo
-  echo '           PROF_TOKEN - user authentication token'
+  echo '           USER_TOKEN - user authentication token'
   exit 1
 fi
 
@@ -20,6 +20,6 @@ BASE_URL=${1}
 curl "${BASE_URL}/profiles" \
   --include \
   --request GET \
-  --header "Authorization: Bearer ${PROF_TOKEN}"
+  --header "Authorization: Bearer ${USER_TOKEN}"
 
 echo
--- curl-scripts/profiles/update.sh
+++ curl-scripts/profiles/update.sh
@@ -8,8 +8,8 @@ then
   echo '  NOTE: Although the BASE_URL is the only variable specified on the command line, the'
   echo '        following variables must also be set in the environment:'
   echo
+  echo '           USER_TOKEN - user authentication token'
   echo '           PROF_NAME - name of the profile'
-  echo '           PROF_TOKEN - user authentication token'
   echo '           PROF_ID - ID of the profile'
   echo '           PROF_DURATION - profile duration (in seconds)'
   echo '           PROF_MINTEMPO - minimum tempo (in beats per minute)'
@@ -22,8 +22,8 @@ BASE_URL=${1}
 
 # echo "PROF_BASE_URL = ${PROF_BASE_URL}"
 # echo "PROF_ID = ${PROF_ID}"
+# echo "USER_TOKEN = ${USER_TOKEN}"
 # echo "PROF_NAME = ${PROF_NAME}"
-# echo "PROF_TOKEN = ${PROF_TOKEN}"
 # echo "PROF_DURATION = ${PROF_DURATION}"
 # echo "PROF_MINTEMPO = ${PROF_MINTEMPO}"
 # echo "PROF_MAXTEMPO = ${PROF_MAXTEMPO}"
@@ -32,7 +32,7 @@ curl "${BASE_URL}/profiles/${PROF_ID}" \
   --include \
   --request PATCH \
   --header "Content-Type: application/json" \
-  --header "Authorization: Bearer ${PROF_TOKEN}" \
+  --header "Authorization: Bearer ${USER_TOKEN}" \
   --data '{
     "profile": {
       "name": "'"${PROF_NAME}"'",
