6c2c64ab3a2c944a015a2410107c766966a848c5
gbbenson68 <gbbenson68@gmail.com>
2019-10-14 14:33:46 -0400

    Add BASH library functions
    Added BASH library functions and shell for new set_end.sh script (in
    progress).

---
+++ curl-scripts/lib/lib_funcs.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Check to see if the given environmental variable exists
+check_env_var() {
+  var=${1}
+  if [ -z $(printenv ${var}) ]
+  then
+    echo 0
+  else
+    echo 1
+  fi
+}
+
+# Check a space-delimited list of environmental variables for existence
+check_list() {
+  list=${1}
+  local retval=1
+  for var in ${list}
+  do
+    retval=$(check_env_var "${var}")
+    if [ ${retval} -eq 0 ]
+    then
+      break
+    fi
+  done
+  echo ${retval}
+}
+
+# List the environmental vbariables that should be specified
+list_vars () {
+  list=${1}
+  for var in ${list}
+  do
+    printf "\t${var}\n"
+  done
+}
