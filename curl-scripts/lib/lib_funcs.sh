92550a06ef68fef6b1a7dea9cace4c45149701ee
gbbenson68 <gbbenson68@gmail.com>
2019-10-15 19:52:27 -0400

    Add get_user_token() function
    Added get_user_token() function to parse response from sign-in and put
    token in a temporary file which can be accessed by all other scripts.

--- curl-scripts/lib/lib_funcs.sh
+++ curl-scripts/lib/lib_funcs.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# USER_TOKEN tempfile prefix
+USER_TOKEN_FILENAME_PRE=/tmp/env-internome-api-USER_TOKEN
+
 # Check to see if the given environmental variable exists
 check_env_var() {
   var=${1}
@@ -33,4 +36,24 @@ list_vars() {
   do
     printf "\t${var}\n"
   done
+}
+
+# Get USER_TOKEN from response
+get_user_token() {
+  return_str="${1}"
+
+  # Derive the check string. I chose to use standard *nix commands here to avoid
+  # the need to install a separate JSON parser just for this processing.
+  # On first sign-in after sign-up, the token value appears earlier in the string,
+  # and therafter later in the string. Hence the if then else statement.
+  check_str=$(echo "${return_str}" | cut -d':' -f2 | sed 's/"//g' | sed 's/^{//')
+
+  if [ "${check_str}" = "token" ]
+  then
+    # First sign-in after sign-up
+    echo "${return_str}" | cut -d':' -f3 | sed 's/"//g' | cut -d',' -f1
+  else
+    # All other sign-ins
+    echo "${return_str}" | cut -d':' -f12 | sed 's/"//g' | sed 's/}//g'
+  fi
 }
